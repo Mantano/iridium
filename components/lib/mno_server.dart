98d0742c85bc4390f584a26d5fe586796c06c797
yteitel <yteitel@mantano.com>
2021-09-20 17:52:02 +0200

    Export asset_provider.dart

--- components/lib/mno_server.dart
+++ components/lib/mno_server.dart
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+export 'src/blocs/server/asset_provider.dart';
 export 'src/blocs/server/assets_request_handler.dart';
 export 'src/blocs/server/fetcher_request_handler.dart';
 export 'src/blocs/server/request_handler.dart';
