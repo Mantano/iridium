7eed59c2954aa0f2ca21d3d3abb1be8d9548ae56
jmgeffroy <jmg@mantano.com>
2021-11-15 17:12:50 +0000

    Deleted navigator

--- navigator/lib/document_opener.dart
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2021 Mantano. All rights reserved.
-// Unauthorized copying of this file, via any medium is strictly prohibited.
-// Proprietary and confidential.
-
-export 'src/document_opener/cbz_document_opener.dart';
-export 'src/document_opener/epub_document_opener.dart';
-export 'src/document_opener/pdf_document_opener.dart';
-export 'src/document_opener/playable_document_opener.dart';
--- navigator/lib/epub.dart
+++
@@ -1,6 +0,0 @@
-// Copyright (c) 2021 Mantano. All rights reserved.
-// Unauthorized copying of this file, via any medium is strictly prohibited.
-// Proprietary and confidential.
-
-export 'src/epub/ui/epub_book_screen.dart';
-export 'src/epub/ui/webview_screen.dart';
