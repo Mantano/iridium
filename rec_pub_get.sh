8dcf150e0683e878a8e2bf0c936cf7bbd9f3294e
jmgeffroy <jmg@mantano.com>
2022-01-03 11:18:06 +0000

    Add dependencies as submodules, last step

--- rec_pub_get.sh
+++ rec_pub_get.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-folders=(
-  "mno_commons_dart"
+folders=("mno_commons_dart"
   "mno_lcp_dart"
   "mno_lcp_native"
   "mno_navigator_flutter"
@@ -8,10 +7,9 @@ folders=(
   "mno_server_dart"
   "mno_shared_dart"
   "mno_streamer_dart"
-#  "pdfium_ffi"
-  "."
+  #  "pdfium_ffi"
 )
 for i in "${folders[@]}"; do
   echo "flutter pub get $i"
-  flutter pub get $i
+  (cd "$i" || exit; flutter pub get)
 done
