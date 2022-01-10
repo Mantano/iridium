ec3d67c03b92377ef7d04c2e5b170e22ef82a1fe
karina_ordonez_flores <kordonez@mantano.com>
2022-01-10 16:29:47 +0100

    GlobalException

---
+++ components/lib/error/exception_type.dart
@@ -0,0 +1,28 @@
+// Copyright (c) 2021 Mantano. All rights reserved.
+// Unauthorized copying of this file, via any medium is strictly prohibited.
+// Proprietary and confidential.
+
+class ExceptionType {
+  static const ExceptionType platform = ExceptionType._(1, "PLATFORM");
+  static const ExceptionType firebaseAuth = ExceptionType._(2, "FIREBASE_AUTH");
+  static const ExceptionType unknown = ExceptionType._(0, "UNKNOWN");
+
+  static const List<ExceptionType> _values = [
+    unknown,
+    platform,
+    firebaseAuth,
+  ];
+  final int id;
+  final String name;
+
+  const ExceptionType._(this.id, this.name);
+
+  static ExceptionType from(int id) =>
+      _values.firstWhere((type) => type.id == id);
+
+  @override
+  List<Object> get props => [id];
+
+  @override
+  String toString() => 'ExceptionType.$name';
+}
